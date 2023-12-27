db9e1babedd582c69368d40e9201ef2bc739737c
joshua <joshuavoo2017@gmail.com>
2023-12-27 19:11:17 +0800

    Added validation for creating new member

--- server/routes/members.mjs
+++ server/routes/members.mjs
@@ -12,6 +12,49 @@ const errorResponse = (message, details = null) => {
 	return { success: false, error: message, details };
 };
 
+// Function to check if fields are empty
+function areFieldsEmpty(member) {
+	for (let key in member) {
+		if (
+			member.hasOwnProperty(key) &&
+			(member[key] === undefined || member[key] === null || member[key] === "")
+		) {
+			return true; // Field is empty
+		}
+	}
+	return false; // No empty fields found
+}
+
+// Funtion to set first character in string to uppercase
+function firstUpperCase(str) {
+	return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function isValidEmail(email) {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return emailRegex.test(email);
+}
+
+function isValidActiveness(activeness) {
+	const value = ["very high", "high", "medium", "low", "very low", "inactive"];
+	return value.includes(activeness.toLowerCase());
+}
+
+function isValidContactNumber(contact_number) {
+	const contactNumberRegex = /^\d{8}$/; // 8 digit contact number
+	return contactNumberRegex.test(contact_number);
+}
+
+function isValidMatrixNumber(matrix_number) {
+	const matrixNumberRegex = /^\d{6}[A-Za-z]$/; // 6 digit before a alphabet
+	return matrixNumberRegex.test(matrix_number);
+}
+
+function isValidYearOfStudy(study_year) {
+	const value = [1, 2, 3];
+	return value.includes(study_year);
+}
+
 // get list of all members info
 router.get("/", async (req, res) => {
 	try {
@@ -50,33 +93,67 @@ router.get("/:id", async (req, res) => {
 // create new member
 router.post("/", async (req, res) => {
 	try {
-		let newMember = {
-			first_name: req.body.first_name,
-			last_name: req.body.last_name,
-			email: req.body.email,
+		const newMember = {
+			first_name: firstUpperCase(req.body.first_name),
+			last_name: firstUpperCase(req.body.last_name),
+			email: req.body.email.toLowerCase(),
 			password: req.body.password,
+			confirmPassword: req.body.confirmPassword,
 			contact_number: req.body.contact_number,
-			matrix_number: req.body.matrix_number,
+			matrix_number: req.body.matrix_number.toUpperCase(),
 			study_year: req.body.study_year,
-			activeness: req.body.activeness,
+			activeness: req.body.activeness.toLowerCase(),
 			request: true,
 		};
-		//need to do vaildation here. and confrim password
 
-		const collection = await db.collection("members");
-		let result = await collection.insertOne(newMember);
+		if (areFieldsEmpty(newMember)) {
+			res
+				.status(400)
+				.send(errorResponse("Please fill in all the input fields."));
+		} else if (!isValidEmail(newMember.email)) {
+			res.status(400).send(errorResponse("Please input a valid email."));
+		} else if (newMember.password !== newMember.confirmPassword) {
+			res
+				.status(400)
+				.send(errorResponse("Password and confirm password does not match."));
+		} else if (!isValidContactNumber(newMember.contact_number)) {
+			res
+				.status(400)
+				.send(errorResponse("Please input a valid contect number."));
+		} else if (!isValidMatrixNumber(newMember.matrix_number)) {
+			res
+				.status(400)
+				.send(errorResponse("Please input a valid matrix card number."));
+		} else if (!isValidYearOfStudy(newMember.study_year)) {
+			res
+				.status(400)
+				.send(errorResponse("Please input a valid year of study(1,2 or 3)."));
+		} else if (!isValidActiveness(newMember.activeness)) {
+			res.status(400).send(errorResponse("Please input a vaild activeness"));
+		} else {
+			delete newMember.confirmPassword;
+			const collection = await db.collection("members");
+			let result = await collection.insertOne(newMember);
 
-		res.status(200).send(successResponse("Member created", result));
+			res.status(200).send(successResponse("Member created", result));
+		}
 	} catch (e) {
-		console.error("Error creating new member:", e);
-		res.status(500).send("Internal Server Error");
+		if (e.code === 11000) {
+			console.error("Matrix number already registered.", e);
+			res
+				.status(500)
+				.send(errorResponse("Matrix number already registered, try another."));
+		} else {
+			console.error("Error creating new member:", e);
+			res.status(500).send(errorResponse("Internal Server Error", e));
+		}
 	}
 });
 
 // update member info by id
 router.patch("/:id", async (req, res) => {
 	try {
-		let updates = {
+		const updates = {
 			$set: {
 				first_name: req.body.first_name,
 				last_name: req.body.last_name,
