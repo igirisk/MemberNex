8141f39d2d33be96703352dd5ddc217d78c94e48
joshua <joshuavoo2017@gmail.com>
2023-12-31 10:25:41 +0800

    Added vaildation for update members

--- server/routes/members.mjs
+++ server/routes/members.mjs
@@ -31,13 +31,17 @@ function firstUpperCase(str) {
 }
 
 function isValidEmail(email) {
+	// one or more characters that is not white space before @, contains @, one or more characters that is not white space after @, contains .,one or more chatacters that matches is not white space till the end of the str
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	return emailRegex.test(email);
 }
 
-function isValidActiveness(activeness) {
-	const value = ["very high", "high", "medium", "low", "very low", "inactive"];
-	return value.includes(activeness.toLowerCase());
+function isValidPassword(password) {
+	// Minimum eight characters, at least one uppercase letter, one lowercase letter, one number, and one special character
+	const passwordRegex =
+		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).{8,}$/;
+
+	return passwordRegex.test(password);
 }
 
 function isValidContactNumber(contact_number) {
@@ -53,6 +57,11 @@ function isValidMatrixNumber(matrix_number) {
 function isValidYearOfStudy(study_year) {
 	const value = [1, 2, 3];
 	return value.includes(study_year);
+}
+
+function isValidActiveness(activeness) {
+	const value = ["very high", "high", "medium", "low", "very low", "inactive"];
+	return value.includes(activeness.toLowerCase());
 }
 
 // get list of all members info
@@ -94,15 +103,15 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
 	try {
 		const newMember = {
-			first_name: firstUpperCase(req.body.first_name),
-			last_name: firstUpperCase(req.body.last_name),
-			email: req.body.email.toLowerCase(),
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+			email: req.body.email,
 			password: req.body.password,
 			confirmPassword: req.body.confirmPassword,
 			contact_number: req.body.contact_number,
-			matrix_number: req.body.matrix_number.toUpperCase(),
+			matrix_number: req.body.matrix_number,
 			study_year: req.body.study_year,
-			activeness: req.body.activeness.toLowerCase(),
+			activeness: req.body.activeness,
 			request: true,
 		};
 
@@ -112,6 +121,14 @@ router.post("/", async (req, res) => {
 				.send(errorResponse("Please fill in all the input fields."));
 		} else if (!isValidEmail(newMember.email)) {
 			res.status(400).send(errorResponse("Please input a valid email."));
+		} else if (!isValidPassword(newMember.password)) {
+			res
+				.status(400)
+				.send(
+					errorResponse(
+						"Password contains at least eight characters, one uppercase letter, one lowercase letter, one number, and one special character."
+					)
+				);
 		} else if (newMember.password !== newMember.confirmPassword) {
 			res
 				.status(400)
@@ -132,6 +149,14 @@ router.post("/", async (req, res) => {
 			res.status(400).send(errorResponse("Please input a vaild activeness"));
 		} else {
 			delete newMember.confirmPassword;
+
+			// formatting values
+			newMember.first_name = firstUpperCase(newMember.first_name);
+			newMember.last_name = firstUpperCase(newMember.last_name);
+			newMember.email = newMember.email.toLowerCase();
+			newMember.matrix_number = newMember.matrix_number.toUpperCase();
+			newMember.activeness = newMember.activeness.toLowerCase();
+
 			const collection = await db.collection("members");
 			let result = await collection.insertOne(newMember);
 
@@ -159,35 +184,111 @@ router.patch("/:id", async (req, res) => {
 				last_name: req.body.last_name,
 				email: req.body.email,
 				password: req.body.password,
+				confirmPassword: req.body.confirmPassword,
 				contact_number: req.body.contact_number,
-				matrix_number: req.body.matrix_number,
 				study_year: req.body.study_year,
 				activeness: req.body.activeness,
-				request: true,
 			},
 		};
-
-		// add confirm password
-		// add form fields vaildation
-
-		const query = { _id: new ObjectId(req.params.id) };
-		const collection = await db.collection("members");
-		let result = await collection.updateOne(query, updates);
-
-		if (result.matchedCount === 1) {
-			if (result.modifiedCount === 1) {
-				res
-					.status(200)
-					.send(successResponse(`${req.params.id} Member is updated`, result));
-			} else {
-				res.status(200).send(successResponse(`Member is up to date`, result));
+		for (let key in updates.$set) {
+			if (
+				updates.$set.hasOwnProperty(key) &&
+				(updates.$set[key] === undefined ||
+					updates.$set[key] === null ||
+					updates.$set[key] === "")
+			) {
+				delete updates.$set[key];
 			}
-		} else {
+		}
+
+		// add form fields vaildation
+		if (Object.keys(updates.$set).length === 0) {
 			res
-				.status(404)
+				.status(400)
+				.send(errorResponse("Please fill in input fields to update."));
+		}
+		if (
+			updates.$set.hasOwnProperty("email") &&
+			!isValidEmail(updates.$set.email)
+		) {
+			res.status(400).send(errorResponse("Please input a valid email."));
+		} else if (
+			updates.$set.hasOwnProperty("password") &&
+			!isValidPassword(updates.$set.password)
+		) {
+			res
+				.status(400)
 				.send(
-					errorResponse("Member not found. Failed to update member", result)
+					errorResponse(
+						"Password must contain at least eight characters, one uppercase letter, one lowercase letter, one number, and one special character."
+					)
 				);
+		} else if (
+			updates.$set.hasOwnProperty("password") &&
+			updates.$set.hasOwnProperty("confirmPassword") &&
+			updates.$set.password !== updates.$set.confirmPassword
+		) {
+			res
+				.status(400)
+				.send(errorResponse("Password and confirm password do not match."));
+		} else if (
+			updates.$set.hasOwnProperty("contact_number") &&
+			!isValidContactNumber(updates.$set.contact_number)
+		) {
+			res
+				.status(400)
+				.send(errorResponse("Please input a valid contect number."));
+		} else if (
+			updates.$set.hasOwnProperty("study_year") &&
+			!isValidYearOfStudy(updates.$set.study_year)
+		) {
+			res
+				.status(400)
+				.send(errorResponse("Please input a valid year of study(1,2 or 3)."));
+		} else if (
+			updates.$set.hasOwnProperty("activeness") &&
+			!isValidActiveness(updates.$set.activeness)
+		) {
+			res.status(400).send(errorResponse("Please input a vaild activeness"));
+		} else {
+			delete updates.$set.confirmPassword;
+			// formatting values
+			const keysToCheck = ["first_name", "last_name", "email", "activeness"];
+
+			keysToCheck.forEach((key) => {
+				if (updates.$set.hasOwnProperty(key)) {
+					// Check the key and perform the corresponding operation
+					if (key === "first_name" || key === "last_name") {
+						updates.$set[key] = firstUpperCase(updates.$set[key]);
+					} else if (key === "email") {
+						updates.$set[key] = updates.$set[key].toLowerCase();
+					} else if (key === "activeness") {
+						updates.$set[key] = updates.$set[key].toLowerCase();
+					}
+				}
+			});
+
+			const query = { _id: new ObjectId(req.params.id) };
+			const collection = await db.collection("members");
+			let result = await collection.updateOne(query, updates);
+
+			if (result.matchedCount === 1) {
+				if (result.modifiedCount === 1) {
+					res
+						.status(200)
+						.send(
+							successResponse(`${req.params.id} Member is updated`, result)
+						);
+				} else {
+					res.status(200).send(successResponse(`Member is up to date`, result));
+				}
+			} else {
+				res
+					.status(404)
+					.send(
+						errorResponse("Member not found. Failed to update member", result)
+					);
+			}
 		}
 	} catch (e) {
 		console.error("Error updating member info:", e);
