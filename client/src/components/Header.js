efb8a18aa55fa5d61609df23e17d81b1d615d841
joshua <joshuavoo2017@gmail.com>
2024-01-03 14:49:22 +0800

    Bootstrap setup done

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const Header = () => {
+	return (
+		<Navbar expand="lg" className="bg-body-tertiary">
+			<Container></Container>
+		</Navbar>
+	);
+};
+
+export default Header;
