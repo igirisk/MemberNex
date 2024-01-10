29faa2bcf1291b5591cc8c666cee9c5673e645ec
joshua <joshuavoo2017@gmail.com>
2024-01-10 23:24:48 +0800

    Completeed popup details for join requests

--- client/src/components/JoinRequests.js
+++ client/src/components/JoinRequests.js
@@ -5,30 +5,89 @@ import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
+import Modal from "react-bootstrap/Modal";
 
 const JoinRequestCard = (props) => {
+	const [showDetails, setShowDetails] = useState(false);
+
+	const handleShowDetails = () => {
+		setShowDetails(true);
+	};
+
+	const handleCloseDetails = () => {
+		setShowDetails(false);
+	};
+
 	return (
-		<Col>
-			<Card style={{ width: "11rem" }}>
-				<Card.Img variant="top" src="holder.js/100px180" />
-				<Card.Body>
-					<Card.Title>
-						{props.joinRequest.first_name + " " + props.joinRequest.last_name}
-					</Card.Title>
-					<Stack gap={1}>
-						<Button variant="success">Accept</Button>
-						<Button
-							variant="danger"
-							onClick={() => {
-								props.rejectJoinRequest(props.joinRequest._id);
-							}}
-						>
-							Reject
-						</Button>
-					</Stack>
-				</Card.Body>
-			</Card>
-		</Col>
+		<>
+			<Col>
+				<Card style={{ width: "11rem" }} onClick={handleShowDetails}>
+					<Card.Img variant="top" src="holder.js/100px180" />
+					<Card.Body>
+						<Card.Title>
+							{props.joinRequest.first_name + " " + props.joinRequest.last_name}
+						</Card.Title>
+						<Stack gap={1}>
+							<Button variant="success">Accept</Button>
+							<Button
+								variant="danger"
+								onClick={() => {
+									props.rejectJoinRequest(props.joinRequest._id);
+								}}
+							>
+								Reject
+							</Button>
+						</Stack>
+					</Card.Body>
+				</Card>
+			</Col>
+			{showDetails && (
+				<JoinRequestDetails
+					joinRequest={props.joinRequest}
+					rejectJoinRequest={props.rejectJoinRequest}
+					onClose={handleCloseDetails}
+				/>
+			)}
+		</>
+	);
+};
+
+const JoinRequestDetails = ({ joinRequest, rejectJoinRequest, onClose }) => {
+	return (
+		<Modal show={true} onHide={onClose}>
+			<Modal.Header closeButton className="px-4">
+				<h2>Join Request</h2>
+			</Modal.Header>
+			<Modal.Body className="px-4">
+				<Row>
+					<Col md="4">
+						<img src="" alt="profile image" />
+						<Stack gap={1}>
+							<Button variant="success">Accept</Button>
+							<Button
+								variant="danger"
+								onClick={() => {
+									rejectJoinRequest(joinRequest._id);
+									onClose(); // close the modal after rejecting
+								}}
+							>
+								Reject
+							</Button>
+						</Stack>
+					</Col>
+					<Col md="8">
+						<Stack gap={1}>
+							<h2>{joinRequest.first_name + " " + joinRequest.last_name}</h2>
+							<p>Email: {joinRequest.email}</p>
+							<p>Contact number: {joinRequest.contact_number}</p>
+							<p>Admin number:{joinRequest.matrix_number}</p>
+							<p>Year of study: {joinRequest.study_year}</p>
+							<p>Activeness: {joinRequest.activeness}</p>
+						</Stack>
+					</Col>
+				</Row>
+			</Modal.Body>
+		</Modal>
 	);
 };
 
